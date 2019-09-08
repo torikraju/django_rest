0cdf402caf028c60b1e0322d9657c6b6c84d8fff
Torikul Alam <torikraju@gmail.com>
2019-09-08 21:29:29 +0600

    Serializer Method Field

--- app_dir/accounts/api/serializers.py
+++ app_dir/accounts/api/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework_jwt.settings import api_settings
+from django.utils import timezone
+from datetime import timedelta
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+jwt_response_payload_handler = api_settings.JWT_RESPONSE_PAYLOAD_HANDLER
+expire_delta = api_settings.JWT_REFRESH_EXPIRATION_DELTA
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
     password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    token = serializers.SerializerMethodField(read_only=True)
+    expires = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = User
@@ -12,9 +23,19 @@ class UserRegisterSerializer(serializers.ModelSerializer):
             'username',
             'email',
             'password',
-            'password2'
+            'password2',
+            'token',
+            'expires'
         ]
         # extra_kwargs = {'password': {'write_only': True}}
+
+    def get_token(self, obj):
+        payload = jwt_payload_handler(obj)
+        token = jwt_encode_handler(payload)
+        return token
+
+    def get_expires(self, obj):
+        return timezone.now() + expire_delta - timedelta(seconds=200)
 
     def validate(self, attrs):
         pw = attrs.get('password')
@@ -27,11 +48,13 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         qs = User.objects.filter(email__iexact=data)
         if qs.exists():
             raise serializers.ValidationError('User with this email already exists')
+        return data
 
     def validate_username(self, data):
         qs = User.objects.filter(username__iexact=data)
         if qs.exists():
             raise serializers.ValidationError('User with this username already exists')
+        return data
 
     def create(self, validated_data):
         user_obj = User(
