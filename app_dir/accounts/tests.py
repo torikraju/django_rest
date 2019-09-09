cffa9a60a04b9c9d22ee19c1d48a3cbb64bd0ba6
Torikul Alam <torikraju@gmail.com>
2019-09-09 22:11:16 +0600

    Automated Testing

--- app_dir/accounts/tests.py
+++ app_dir/accounts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from faker import Factory
 
-# Create your tests here.
+faker = Factory.create()
+profile = faker.profile(fields=None, sex=None)
+
+
+class StatusTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(username=profile['username'], email=profile['mail'])
+        user.set_password('1')
+        user.save()
+
+    def test_created_user(self):
+        qs = User.objects.filter(username=profile['username'])
+        self.assertEqual(qs.count(), 1)
--- app_dir/status/tests.py
+++ app_dir/status/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from faker import Factory
 
-# Create your tests here.
+from .models import Status
+
+faker = Factory.create()
+profile = faker.profile(fields=None, sex=None)
+
+
+class StatusTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(username=profile['username'], email=profile['mail'])
+        user.set_password('1')
+        user.save()
+
+    def test_created_status(self):
+        user = User.objects.get(username=profile['username'])
+        status = Status.objects.create(
+            user=user,
+            content=faker.paragraphs(nb=3, ext_word_list=None)
+        )
+        self.assertEqual(status.id, 1)
+        qs = Status.objects.all()
+        self.assertEqual(qs.count(), 1)
